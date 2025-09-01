cfda691057b2e9ddaa1179f8ff53f4bc47fb7fe7
Chawawnit078 <Chawawnit.khu@student.mahidol>
2025-09-01 21:32:16 +0700

    create backend folder, springboot with docker-compose

---
+++ backend/src/test/java/com/organicnow/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.organicnow.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
