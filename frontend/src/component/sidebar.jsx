4fa762e3b76293af37f847c6a1610d8463ab2983
kkkkhxx <tanyaratclub@gmail.com>
2025-09-10 13:13:28 +0700

    commit test

--- frontend/src/component/sidebar.jsx
+++ frontend/src/component/sidebar.jsx
@@ -16,8 +16,8 @@ export default function SideBar() {
         <NavLink to="/" end className={linkClass}><i className="pi pi-home icon-lg" /></NavLink>
         <NavLink to="/test" className={linkClass}><i className="pi pi-cog icon-lg" /></NavLink>
         <NavLink to="/test2" className={linkClass}><i className="pi pi-user icon-lg" /> </NavLink>
-        
       </nav>
+
     </aside>
   );
 }
