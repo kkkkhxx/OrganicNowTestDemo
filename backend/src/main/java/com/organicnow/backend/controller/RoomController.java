7fbc14b1ac32c33f890b7c6204e3bf82b3b6f0e3
Chawanwit078 <Chawanwit.khu@student.mahidol.edu>
2025-09-11 17:08:38 +0700

    worked docker-compose

--- backend/src/main/java/com/organicnow/backend/controller/RoomController.java
+++ backend/src/main/java/com/organicnow/backend/controller/RoomController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/rooms")
 @CrossOrigin(origins = "http://localhost:5173") // อนุญาต frontend
-public class RoomController {
+public class  RoomController {
 
     private final RoomService roomService;
 
